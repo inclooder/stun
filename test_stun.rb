ecaf1c54d318662a9dfc8d8242489f04d5b3f071
Mateusz Kluge <inclooder@gmail.com>
2020-05-01 18:55:58 +0200

    Use bindata

--- test_stun.rb
+++ test_stun.rb
@@ -1,5 +1,7 @@
+#!/usr/bin/ruby
 require 'socket'
 require 'securerandom'
+require 'bindata'
 
 #dokumentacja https://tools.ietf.org/html/rfc3489
 
@@ -21,6 +23,85 @@ servers = [
   { host: host, port: port.to_i }
 end
 
+class StunMessageHeader < BinData::Record
+  MESSAGE_TYPES = {
+    0x0001 => :binding_request,
+    0x0101 => :binding_response,
+    0x0111 => :binding_error_response,
+    0x0002 => :shared_secret_request,
+    0x0102 => :shared_secret_response,
+    0x0112 => :shared_secret_error_response
+  }.freeze
+
+  endian :big
+
+  uint16 :message_type
+  uint16 :message_length
+  string :transaction_id, length: 16
+
+  def message_type_name
+    MESSAGE_TYPES.fetch(message_type)
+  end
+end
+
+class AddressAttributeValue < BinData::Record
+  endian :big
+
+  skip length: 1
+  uint8 :family
+  uint16 :port
+  uint8 :ip_a
+  uint8 :ip_b
+  uint8 :ip_c
+  uint8 :ip_d
+
+  def ip
+    [ip_a, ip_b, ip_c, ip_d].map(&:to_s).join('.')
+  end
+end
+
+class MappedAddress < AddressAttributeValue
+end
+
+class ResponseAddress < AddressAttributeValue
+end
+
+class StunMessageAttribute < BinData::Record
+  VALUE_TYPES = {
+    0x0001 => :mapped_address,
+    0x0002 => :response_address,
+    0x0003 => :change_request,
+    0x0004 => :source_address,
+    0x0005 => :changed_address,
+    0x0006 => :username,
+    0x0007 => :password,
+    0x0008 => :message_integrity,
+    0x0009 => :error_code,
+    0x000A => :unknown_attributes,
+    0x000B => :reflected_from,
+  }.freeze
+
+  endian :big
+
+  uint16 :value_type
+  uint16 :value_length
+  # buffer :attribute_value, type: :uint8, length: :value_length
+  choice :attribute_value, selection: lambda { value_type }, choices: {
+    0x0001 => :mapped_address,
+    0x0002 => :response_address,
+  }
+
+  def value_type_name
+    VALUE_TYPES.fetch(value_type)
+  end
+end
+
+class StunMessage < BinData::Record
+  endian :big
+  stun_message_header :header
+  buffer :attributes, type: :stun_message_attribute, length: lambda { header.message_length }
+end
+
 class StunHeader
   MESSAGE_TYPES = {
     binding_request: 0x001,
@@ -37,7 +118,6 @@ class StunHeader
     @message_type = type
     @message_length = length
     @transaction_id = transaction_id
-    puts transaction_id
   end
 
   def to_data
@@ -51,13 +131,6 @@ class StunHeader
   def self.from_data(data)
     message_type, message_length, transaction_id = data.unpack('nna16')
     new(message_type, message_length, transaction_id)
-  end
-end
-
-class StunAttribute
-  attr_reader :type, :length, :value
-
-  def self.from_data(data)
   end
 end
 
@@ -79,8 +152,8 @@ socket.connect(host, port)
 puts socket.inspect
 socket.send(payload, 0, host, port)
 resp = socket.recv(1024)
+msg = StunMessage.read(resp)
+puts "Response #{msg.inspect}"
+puts "MessageType #{msg.header.message_type_name}"
+
 puts "Response from #{iden} #{resp.inspect}"
-
-resp_header = resp[0...20]
-puts StunHeader.from_data(resp_header).inspect
-
